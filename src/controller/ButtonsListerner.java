afffb043b9a2fc2efa0acfb793fedd59ac371881
Joseph Nguyen <joseph.nguyen.90@gmail.com>
2012-05-10 12:52:18 +0200

    Ajout Package controller et Déplacement ButtonsListerner

---
+++ src/controller/ButtonsListerner.java
@@ -0,0 +1,17 @@
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import others.Request;
+
+public class ButtonsListerner implements ActionListener{
+	
+	Request r = new Request();
+	
+	public void actionPerformed(ActionEvent ae) {
+		r.showChar(ae.getActionCommand());
+		r.showMove(ae.getActionCommand());
+	}
+	
+}
